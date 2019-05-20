b958771b5f7e8ebf7bfb5618a1e4f989b37ed224
Oliver Jägle <github@beimir.net>
2019-05-20 16:44:18 +0200

    Search feature: Apply filter to data binding

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,11 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/base/Log",
-	"../model/formatter"
-], function (Controller, Log, formatter) {
+	"../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, Log, formatter, Filter, FilterOperator) {
+
 	"use strict";
 
 	return Controller.extend("oj.movies.controller.App", {
@@ -13,9 +16,35 @@ sap.ui.define([
 		onInit: function () {
 
 		},
-		onPressFindMovies: function (sCity, oEvent){
-			sap.ui.require(["sap/m/MessageToast"], function(Toast){
-				Toast.show("Searching...");
+		findMovies: function () {
+
+			var sCity = this.byId('city').getValue(),
+				sGenre = this.byId('genre').getSelectedItem().getKey(),
+				oCalendar = this.byId("calendar"),
+				oRowBinding = oCalendar.getBinding("rows"),
+				oFilterGenre,
+				oFilterCity;
+
+			// Produce a message so that the user knows for sure the button was interacted with
+			sap.ui.require(["sap/m/MessageToast"], function (Toast) {
+				var oResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+				sCity 
+					? Toast.show(`${oResourceBundle.getText("search")} "${sCity}"...`)
+					: Toast.show(`${oResourceBundle.getText("search")}...`);
+			}.bind(this));
+
+			// Create filters for genre and city according to user inputs
+			oFilterGenre = sGenre ? new Filter("genre", FilterOperator.EQ, sGenre) : null;
+			oFilterCity = sCity ? new Filter("info", FilterOperator.Contains, sCity) : null;
+
+			// Apply genre filter to calendar rows (movies)
+			oRowBinding.filter(oFilterGenre);
+
+			// Apply city filter to row appointments - This will NOT hide movies without mathing appointments
+			var aRows = oCalendar.getAggregation("rows");
+			aRows.forEach(function (oItem) {
+				var oAppointmentsBinding = oItem.getBinding("appointments");
+				oAppointmentsBinding.filter(oFilterCity);
 			});
 		}
 	});
